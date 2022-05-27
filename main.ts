35e028339ddf8eb258b720f0f55f4689380c1b53
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2022-05-27 14:25:47 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,19 +3,24 @@ let atala_1 = 1
 let atal_2 = 0
 let atala_3 = 0
 basic.forever(function () {
-    if (atal_2 == 1) {
-        basic.showNumber(1)
-        if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 30) {
+    if (atala_1 == 1) {
+        if (0 == (false && false)) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 15) {
+        }
+    }
+})
+basic.forever(function () {
+    if (atala_3 == 1) {
+        if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 30) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 15) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
-        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 10) {
+        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 10) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 25)
         } else {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
             basic.showIcon(IconNames.Ghost)
-            atala_1 = 0
-            atal_2 = 1
+            atala_3 = 0
         }
     }
 })
