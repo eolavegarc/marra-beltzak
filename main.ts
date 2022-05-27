e3517c22507df24669c484f4a3dfc7d41f7a5dbe
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2022-05-27 14:00:38 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
+DFRobotMaqueenPlus.I2CInit()
+let atala_1 = 1
+let atal_2 = 0
+let atala_3 = 0
 basic.forever(function () {
-    let atal_2 = 0
     if (atal_2 == 1) {
-        if (0 == 1 == (0 == 1)) {
-        	
+        basic.showNumber(1)
+        if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 30) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 15) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+        } else if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) == 10) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 25)
+        } else {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+            basic.showIcon(IconNames.Ghost)
+            atala_1 = 0
+            atal_2 = 1
         }
     }
 })
