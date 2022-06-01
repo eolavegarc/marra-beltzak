5ad62eb78b4ad510838f3337aa821ee8060a5155
eolavegarc <93262443+eolavegarc@users.noreply.github.com>
2022-06-01 14:27:09 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,6 @@ let atala_1 = 1
 let atal_2 = 0
 let atala_3 = 0
 basic.forever(function () {
-    if (atala_1 == 1) {
-        if (0 == (false && false)) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
-        }
-    }
-})
-basic.forever(function () {
     if (atala_3 == 1) {
         if (DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2) > 30) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
@@ -22,5 +15,37 @@ basic.forever(function () {
             basic.showIcon(IconNames.Ghost)
             atala_3 = 0
         }
+    }
+})
+basic.forever(function () {
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1) {
+    	
+    }
+})
+basic.forever(function () {
+    if (atala_1 == 1) {
+        if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+        } else {
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 0 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 1) {
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
+            }
+            if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) == 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) == 0) {
+                DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+                DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 100)
+            }
+        }
+    }
+})
+basic.forever(function () {
+    if (atal_2 == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+        basic.pause(1000)
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        basic.showNumber(2)
+        basic.pause(1000)
+        atal_2 = 0
+        atal_2 = 1
     }
 })
